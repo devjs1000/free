27c53321d6165f0c8c63d1622c093785d4495d26
vegtazain <93030684+vegtazain@users.noreply.github.com>
2021-10-23 14:37:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { div, info, render, tag, hide, show, design, select } from '/component/apk.js'
-import {log} from '/component/log.js'
-import {nav} from '/component/nav.js'
-import {bottomNav} from '/component/bottomNav.js'
-import {home} from '/component/home.js'
-import {cart} from '/component/cart.js'
+import { div, info, render, tag, hide, show, design, select } from 'component/apk.js'
+import {log} from 'component/log.js'
+import {nav} from 'component/nav.js'
+import {bottomNav} from 'component/bottomNav.js'
+import {home} from 'component/home.js'
+import {cart} from 'component/cart.js'
 
 hide('#log')
 hide('#about')
